4c6252aa6b35fe931af1d0069561eaa4714686e0
Nathan <hildebrn@oregonstate.edu>
2018-06-11 14:54:20 -0700

    Failing: game has grid

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,15 @@
 require_relative '../lib/game'
 
 describe "Conway's game of life" do
+  let(:game) {Game.new}
 
   it 'exists' do
     Game.new
   end
 
-  xit 'is instantiated with a grid' do
-
+  it 'is instantiated with a grid' do
+    game.instantiate
+    exect(game).to respond_to(:grid)
   end
 
   xit 'provides a way to obtain the grid it was instantiated with' do
